9f691261e02fbf8d5114aac3c810cf1492dd8705
limengjie <li.mengjie@yalla.live>
2019-11-18 11:48:01 +0800

    add project

---
+++ DynamicPlugins/Data/MyContext.cs
@@ -0,0 +1,27 @@
+﻿using DynamicPlugins.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace DynamicPlugins.Data
+{
+    public class MyContext : DbContext
+    {
+        public MyContext(DbContextOptions<MyContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Plugin> Plugins { get; set; }
+        public DbSet<PluginMigration> PluginMigrations { get; set; }
+    }
+
+    public class DataContextFactory : IDesignTimeDbContextFactory<MyContext>
+    {
+        public MyContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<MyContext>();
+            builder.UseMySql("server=127.0.0.1;port=3306;database=pluging;uid=root;pwd=password;");
+            return new MyContext(builder.Options);
+        }
+    }
+}
---
+++ DynamicPlugins/Migrations/20191118030551_Init.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DynamicPlugins.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PluginMigrations",
+                columns: table => new
+                {
+                    PluginMigrationId = table.Column<Guid>(nullable: false),
+                    PluginId = table.Column<Guid>(nullable: false),
+                    Version = table.Column<string>(nullable: true),
+                    Up = table.Column<string>(nullable: true),
+                    Down = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PluginMigrations", x => x.PluginMigrationId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Plugins",
+                columns: table => new
+                {
+                    PluginId = table.Column<Guid>(nullable: false),
+                    UniqueKey = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    DisplayName = table.Column<string>(nullable: true),
+                    Version = table.Column<string>(nullable: true),
+                    IsEnable = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Plugins", x => x.PluginId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PluginMigrations");
+
+            migrationBuilder.DropTable(
+                name: "Plugins");
+        }
+    }
+}
